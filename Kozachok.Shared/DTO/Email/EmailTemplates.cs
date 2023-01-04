f5756b80508665019ac3cd1a230ecb1859cf1858
ihorb <ihorb@ipoft.com>
2023-01-04 04:57:42 +0200

    Changed E-mail sending functionality

---
+++ Kozachok.Shared/DTO/Email/EmailTemplates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kozachok.Shared.DTO.Email
+{
+    public class EmailTemplates
+    {
+        public const int EmailConfirmation = 2;
+        public const int PasswordReset = 3;
+        public const int EmailReset = 4;
+    }
+}
