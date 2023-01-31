5e870f74807c86e83c21406daf825f2f7447e3c8
ihorbandura <ihor.bandura@softserveinc.com>
2023-01-31 17:09:06 +0200

    Added Script Progress to get current progress of script running on kozachok server

---
+++ Kozachok.Domain/Handlers/Queries/GetScriptProgressQuery.cs
@@ -0,0 +1,13 @@
+﻿using Kozachok.Shared.Abstractions.Commands;
+using Kozachok.Shared.DTO.Models.DbEntities;
+using Kozachok.Shared.DTO.Models.Result.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kozachok.Domain.Handlers.Queries
+{
+    public class GetScriptProgressQuery : RequestCommand<ScriptProgress>
+    {
+    }
+}
---
+++ Kozachok.WebApi/Controllers/ServerController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Kozachok.Domain.Commands.User;
+using Kozachok.Domain.Handlers.Queries;
+using Kozachok.Domain.Queries.User;
+using Kozachok.Shared.Abstractions.Bus;
+using Kozachok.Shared.DTO.Common;
+using Kozachok.Shared.DTO.Models.DbEntities;
+using Kozachok.Shared.DTO.Models.Result.Email;
+using Kozachok.WebApi.Controllers.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kozachok.WebApi.Controllers
+{
+    public class ServerController : UserControllerBase
+    {
+        public ServerController(
+            IMediatorHandler mediator,
+            IMapper mapper,
+            INotificationHandler<DomainNotification> notifications
+            ) : base(mediator, mapper, notifications)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Post()
+        {
+            var result = await bus.RequestAsync<ScriptProgress>(new GetScriptProgressQuery());
+            return Response(result);
+        }
+    }
+}
