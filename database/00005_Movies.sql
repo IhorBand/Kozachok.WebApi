bf9e032e00860a8f98c7b174446aed0aa2e1d464
Ihor Bandura <igor.bandura.3@gmail.com>
2023-04-27 06:10:57 +0300

    Movie Database

---
+++ database/00005_Movies.sql
@@ -0,0 +1,236 @@
+﻿CREATE TABLE [dbo].[L_Country] (
+	[Id] int NOT NULL,
+	[Name] nvarchar(200) NOT NULL,
+	TranslationKey uniqueidentifier NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_L_Country_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CONSTRAINT L_Country_Id PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.L_Movie_Genre (
+	Id int NOT NULL,
+	[Value] nvarchar(200) NOT NULL,
+	TranslationKey uniqueidentifier NULL,
+	CONSTRAINT L_Movie_Genre_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.L_Movie_Main_Category (
+	Id tinyint NOT NULL,
+	[Value] nvarchar(200) NOT NULL,
+	CONSTRAINT L_Movie_Main_Category_Id PRIMARY KEY CLUSTERED
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.L_Movie_Type (
+	Id tinyint NOT NULL,
+	[Value] nvarchar(200) NOT NULL,
+	CONSTRAINT L_Movie_Type_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.L_Translation_Language (
+	Id int IDENTITY(1,1) NOT NULL,
+	[Name] nvarchar(100) NULL,
+	FullName varchar(100) NULL,
+	IsoCode varchar(5) NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_L_Translation_Language_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CONSTRAINT L_Translation_Language_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE [dbo].[L_Translator] (
+	[Id] UNIQUEIDENTIFIER CONSTRAINT DF_L_Translator_Id DEFAULT newid() NOT NULL,
+	[Name] nvarchar(400) NOT NULL,
+	TranslatorID nvarchar(100) NULL,
+	LanguageId INT NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_L_Translator_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CONSTRAINT L_Translator_Id PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.T_Movie (
+	Id uniqueidentifier CONSTRAINT DF_T_Movie_Id DEFAULT newid() NOT NULL,
+	VideoId varchar(200) NULL,
+	VideoUrlId varchar(400) NULL,
+	AdditionalCoverUrl varchar(400) NULL,
+	ShortTitle nvarchar(300) NULL,
+	FullTitle nvarchar(300) NULL,
+	OriginalFullTitle nvarchar(500) NULL,
+	ShortDescription nvarchar(300) NULL,
+	FullDescription nvarchar(MAX) NULL,
+	ImdbId varchar(200) NULL,
+	ImdbUrl varchar(400) NULL,
+	ImdbCoverUrl varchar(400) NULL,
+	ImdbDescription nvarchar(MAX) NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_T_Movie_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	VideoFullUrl varchar(400) NULL,
+	TypeId tinyint NULL,
+	MainCategoryId tinyint NULL,
+	ReleaseDate datetime NULL,
+	CONSTRAINT T_Movie_Id PRIMARY KEY CLUSTERED
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.T_Movie_Actor (
+	Id uniqueidentifier CONSTRAINT DF_T_Movie_Actor_Id DEFAULT newid() NOT NULL,
+	FirstName nvarchar(200) NULL,
+	LastName nvarchar(200) NULL,
+	ImageUrl varchar(400) NULL,
+	MovieId uniqueidentifier NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_T_Movie_Actor_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CONSTRAINT T_Movie_Actor_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.T_Movie_Country (
+	Id uniqueidentifier CONSTRAINT DF_T_Movie_Country_Id DEFAULT newid() NOT NULL,
+	MovieId uniqueidentifier NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_T_Movie_Country_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CountryId int NULL,
+	CONSTRAINT T_Movie_Country_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.T_Movie_Director (
+	Id uniqueidentifier CONSTRAINT DF_T_Movie_Director_Id DEFAULT newid() NOT NULL,
+	FirstName nvarchar(200) NULL,
+	LastName nvarchar(200) NULL,
+	MovieId uniqueidentifier NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_T_Movie_Director_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CONSTRAINT T_Movie_Rezka_Director_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.T_Movie_Genre (
+	Id uniqueidentifier CONSTRAINT DF_T_Movie_Genre_Id DEFAULT newid() NOT NULL,
+	MovieId uniqueidentifier NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_T_Movie_Genre_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	GenreId int NULL,
+	CONSTRAINT T_Movie_Rezka_Genre_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.T_Movie_Image (
+	Id uniqueidentifier CONSTRAINT DF_T_Movie_Image_Id DEFAULT newid() NOT NULL,
+	[Value] varchar(400) NULL,
+	MovieId uniqueidentifier NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_T_Movie_Image_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CONSTRAINT T_Movie_Image_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.T_Movie_Translator (
+	Id uniqueidentifier CONSTRAINT DF_T_Movie_Translator_Id DEFAULT newid() NOT NULL,
+	MovieId uniqueidentifier NULL,
+	TranslatorId UNIQUEIDENTIFIER NOT NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_T_Movie_Translator_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CONSTRAINT T_Movie_Translator_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+CREATE TABLE dbo.T_Translation (
+	Id uniqueidentifier CONSTRAINT DF_T_Translation_Id DEFAULT newid() NOT NULL,
+	[Key] uniqueidentifier CONSTRAINT DF_T_Translation_Key DEFAULT newid() NOT NULL,
+	TranslationLanguageId int NOT NULL,
+	[Value] nvarchar(500) NULL,
+	CreatedDateUTC datetime CONSTRAINT DF_T_Translation_CreatedDateUTC DEFAULT getutcdate() NOT NULL,
+	CONSTRAINT T_Translation_Id PRIMARY KEY CLUSTERED 
+	(
+		Id ASC
+	)
+);
+GO
+
+ALTER TABLE dbo.T_Movie 
+ADD CONSTRAINT FK_T_Movie_MainCategoryId_To_L_Movie_Main_Category_Id 
+FOREIGN KEY (MainCategoryId) 
+REFERENCES dbo.L_Movie_Main_Category(Id);
+GO
+
+ALTER TABLE dbo.T_Movie 
+ADD CONSTRAINT FK_T_Movie_TypeId_To_L_Movie_Type_Id 
+FOREIGN KEY (TypeId) 
+REFERENCES dbo.L_Movie_Type(Id);
+GO
+
+ALTER TABLE dbo.T_Movie_Country 
+ADD CONSTRAINT FK_T_Movie_Country_CountryId_To_L_Country_Id 
+FOREIGN KEY (CountryId) 
+REFERENCES dbo.L_Country(Id);
+GO
+
+ALTER TABLE dbo.T_Movie_Country 
+ADD CONSTRAINT FK_T_Movie_Country_MovieId_To_T_Movie_Id 
+FOREIGN KEY (MovieId) 
+REFERENCES dbo.T_Movie(Id);
+GO
+
+ALTER TABLE dbo.T_Movie_Genre 
+ADD CONSTRAINT FK_T_Movie_Genre_GenreId_To_L_Movie_Genre_Id 
+FOREIGN KEY (GenreId) 
+REFERENCES dbo.L_Movie_Genre(Id);
+GO
+
+ALTER TABLE dbo.T_Translation 
+ADD CONSTRAINT FK_T_Translation_TranslationLanguageId_To_L_Translation_Language_Id 
+FOREIGN KEY (TranslationLanguageId) 
+REFERENCES dbo.L_Translation_Language(Id);
+GO
+
+ALTER TABLE [dbo].[L_Translator]
+ADD CONSTRAINT FK_L_Translator_LanguageId_To_L_Translation_Language_Id 
+FOREIGN KEY (LanguageId) 
+REFERENCES dbo.L_Translation_Language(Id);
+GO
+
+ALTER TABLE dbo.T_Movie_Translator 
+ADD CONSTRAINT FK_T_Movie_Translator_TranslatorId_To_L_Translator_Id 
+FOREIGN KEY (TranslatorId) 
+REFERENCES dbo.L_Translator(Id);
+GO
+
+ALTER TABLE dbo.T_Movie_Translator 
+ADD CONSTRAINT FK_T_Movie_Translator_MovieId_To_T_Movie_Id 
+FOREIGN KEY (MovieId) 
+REFERENCES dbo.T_Movie(Id);
+GO
---
+++ database/00007_Movies_Procedures.sql
@@ -0,0 +1,153 @@
+﻿CREATE PROC [dbo].[InsertNewTranslator] 
+	@MovieId [UNIQUEIDENTIFIER],
+	@TranslatorName [NVARCHAR](400) NULL,
+	@TranslatorId [VARCHAR](100) NULL,
+	@TranslatorLanguage [NVARCHAR](300) NULL
+AS
+BEGIN
+	INSERT INTO [dbo].[T_Movie_Translator_Bak](
+		MovieId,
+		TranslatorName,
+		TranslatorId,
+		TranslatorLanguage)
+	OUTPUT Inserted.Id
+	VALUES(
+		@MovieId,
+		@TranslatorName,
+		@TranslatorId,
+		@TranslatorLanguage
+		);
+
+	IF @TranslatorName IS NULL OR  @TranslatorName = '' OR (@TranslatorName IS NOT NULL AND @TranslatorName <> '' AND ISNUMERIC(@TranslatorId) = 0)
+	BEGIN
+		-- not available
+		INSERT INTO T_Movie_Translator (MovieId, TranslatorId) VALUES(@MovieId, (SELECT TOP 1 lt.Id FROM L_Translator lt WHERE lt.TranslatorID = 'NULL'))
+	END
+	ELSE IF EXISTS (SELECT * FROM L_Translator lt WHERE lt.TranslatorID = @TranslatorID)
+	BEGIN 
+		-- available, need to link by id
+		INSERT INTO T_Movie_Translator (MovieId, TranslatorId) VALUES(@MovieId, (SELECT TOP 1 lt.Id FROM L_Translator lt WHERE lt.TranslatorID = @TranslatorID))
+	END
+	ELSE
+	BEGIN
+		-- available, but need to add new record in L_Translator
+		DECLARE @TranslatorLanguageId INT =
+		(
+			SELECT CASE 
+				WHEN @TranslatorLanguage = N'Украинский' THEN 1
+				WHEN @TranslatorLanguage = N'Казахский' THEN 4
+				WHEN @TranslatorLanguage IS NULL THEN 3
+				ELSE 3
+				END
+		)
+		
+		DECLARE @InsertedId table ( Id UNIQUEIDENTIFIER )
+		
+		INSERT INTO L_Translator([Name], [TranslatorId], [LanguageId]) 
+		OUTPUT inserted.Id INTO @InsertedId
+		VALUES(@TranslatorName, @TranslatorId, @TranslatorLanguageId)
+		
+		INSERT INTO T_Movie_Translator(MovieId, TranslatorId) VALUES(@MovieId, (SELECT TOP 1 Id FROM @InsertedId))
+	END
+	
+END
+GO
+
+CREATE PROCEDURE sp_SearchMovies
+	@SearchValue NVARCHAR(200) = NULL,
+    @PageIndex INT = 1,
+    @PageSize INT = 10,
+    @GenreId INT = 0,
+    @CountryId INT = 0,
+    @TotalCount INT OUTPUT,
+    @TotalPages INT OUTPUT
+WITH RECOMPILE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	DECLARE @sql NVARCHAR(MAX) = ''
+   
+	SET @TotalCount = 0
+	SET @TotalPages = 1
+
+	IF @PageIndex <= 0
+	BEGIN
+		SET @PageIndex = 1
+	END
+
+	IF @PageSize < 10
+	BEGIN
+		SET @PageSize = 10
+	END
+		
+	SET @sql = '
+		IF OBJECT_ID (''tempdb..#searchMovieQuery'') IS NOT NULL
+			DROP TABLE #searchMovieQuery
+	
+		SELECT
+			[movie].*
+		INTO #searchMovieQuery
+		FROM [T_Movie] movie
+		WHERE (LOWER(FullTitle) LIKE ''%'' + @SearchValue + ''%'' 
+			OR LOWER(OriginalFullTitle) LIKE ''%'' + @SearchValue + ''%'')'
+	
+	IF @GenreId > 0
+	BEGIN
+		SET @sql += 'AND EXISTS (SELECT Id FROM T_Movie_Genre mg WHERE mg.MovieId = movie.Id AND mg.GenreId = @GenreId)'
+	END
+	
+	IF @CountryId > 0
+	BEGIN
+		SET @sql += 'AND EXISTS (SELECT Id FROM T_Movie_Country mc WHERE mc.MovieId = movie.Id AND mc.CountryId = @CountryId)'
+	END
+	
+	SET @sql += '
+	
+		SELECT @TotalCount = COUNT(*) FROM #searchMovieQuery
+	
+		SELECT @TotalPages = CEILING(@TotalCount / CAST(@PageSize AS FLOAT))
+	
+		SELECT * 
+		FROM #searchMovieQuery
+		ORDER BY CreatedDateUTC
+		OFFSET (@PageIndex - 1) * @PageSize ROWS	-- skip  rows
+		FETCH NEXT @PageSize ROWS ONLY;				-- take 10 rows
+	
+		DROP TABLE #searchMovieQuery'
+	
+	EXECUTE sp_executesql @sql, 
+		N'@SearchValue NVARCHAR(200), 
+		@PageSize INT, 
+		@PageIndex INT, 
+		@GenreId INT,
+		@CountryId INT,
+		@TotalCount INT OUTPUT, 
+		@TotalPages INT OUTPUT', 
+		@SearchValue, 
+		@PageSize, 
+		@PageIndex,
+		@GenreId,
+		@CountryId,
+		@TotalCount OUTPUT, 
+		@TotalPages OUTPUT
+		
+	SELECT @TotalCount, @TotalPages
+END
+GO
+
+CREATE PROCEDURE sp_SearchMovies_Top
+	@SearchValue NVARCHAR(200) = NULL
+WITH RECOMPILE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	SELECT TOP 5
+		[movie].*
+	FROM [T_Movie] movie
+	WHERE LOWER([RezkaFullTitle]) LIKE '%' + @SearchValue + '%' 
+		OR LOWER([RezkaOriginalFullTitle]) LIKE '%' + @SearchValue + '%'
+	ORDER BY [CreatedDateUTC] DESC
+END
+GO
