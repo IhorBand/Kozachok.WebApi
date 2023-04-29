0858caeefa80e0219ffaf4e4f2fb9d553e2a817c
Ihor Bandura <igor.bandura.3@gmail.com>
2023-04-29 06:00:31 +0300

    Warning Fix

--- Kozachok.WebApi/Models/Movie/Movie.cs
+++ Kozachok.WebApi/Models/Movie/Movie.cs
@@ -4,16 +4,16 @@ namespace Kozachok.WebApi.Models.Movie
 {
     public class Movie
     {
-        public string VideoId { get; set; }
-        public string ShortTitle { get; set; }
-        public string FullTitle { get; set; }
-        public string OriginalFullTitle { get; set; }
-        public string ShortDescription { get; set; }
-        public string FullDescription { get; set; }
-        public string ImdbId { get; set; }
-        public string ImdbUrl { get; set; }
-        public string ImdbCoverUrl { get; set; }
-        public string ImdbDescription { get; set; }
+        public string? VideoId { get; set; }
+        public string? ShortTitle { get; set; }
+        public string? FullTitle { get; set; }
+        public string? OriginalFullTitle { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? FullDescription { get; set; }
+        public string? ImdbId { get; set; }
+        public string? ImdbUrl { get; set; }
+        public string? ImdbCoverUrl { get; set; }
+        public string? ImdbDescription { get; set; }
         public MovieType TypeId { get; set; }
         public MovieMainCategory MainCategoryId { get; set; }
         public DateTime ReleaseDate { get; set; }
